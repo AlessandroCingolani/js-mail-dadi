cdb2a68c45c126ad69995abd24d28791678e7347
AlessandroCingolani <alessandro.cingolani93@gmail.com>
2023-09-12 15:57:51 +0200

    [add]:conditions win draw

--- Dicegame/js/script.js
+++ Dicegame/js/script.js
@@ -4,10 +4,18 @@ const dice = [1,2,3,4,5,6];
 
 console.log(dice);
 
-const randomNumber = Math.floor(Math.random() * dice.length);
+let randomNumber = Math.floor(Math.random() * dice.length);
 
 playerRoll = dice[randomNumber];
 console.log(playerRoll);
 
 computerRoll = dice[randomNumber];
 console.log(computerRoll);
+
+if (playerRoll === computerRoll){
+  console.log('DRAW');
+}else if (playerRoll < computerRoll){
+  console.log('Computer WIN');
+}else {
+  console.log('Player WIN');
+}
